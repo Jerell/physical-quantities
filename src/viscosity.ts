3c7f558be29b3e8a809f5f603f3f116a49a46913
Jerell James <jerell1998@gmail.com>
2023-08-09 09:27:02 +0100

    viscosity

---
+++ src/viscosity.ts
@@ -0,0 +1,34 @@
+import { PhysicalQuantity } from './physicalQuantity'
+
+export class Viscosity extends PhysicalQuantity {
+  private _pas: number
+
+  constructor(n: number, unitString: string) {
+    super('viscosity')
+    const unit: ViscosityUnits = unitString as ViscosityUnits
+    switch (unit) {
+      case ViscosityUnits.Pas:
+        this._pas = n
+        break
+      default:
+        throw new Error('Unit not supported')
+    }
+  }
+
+  get Pas() {
+    return this._pas
+  }
+
+  getNumber(unit: string): number {
+    switch (unit) {
+      case ViscosityUnits.Pas:
+        return this.Pas
+      default:
+        return -1
+    }
+  }
+}
+
+export enum ViscosityUnits {
+  Pas = 'Pa·s',
+}
